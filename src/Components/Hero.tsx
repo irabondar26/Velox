cfe036bc660d1132b1717c4d26c2ecfa3f051ced
Ira Bondar <irabondar1012@gmail.com>
2026-03-31 16:47:36 +0200

    change the right position of an another element

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -19,7 +19,7 @@ export default function Hero() {
                 </div>
                 <p className="text-xs md:text-base text-white">Payment recieved!</p>
             </div>
-            <div className="hidden absolute top-[35%] right-[10%] py-1 pl-1 pr-2 rounded-full justify-center items-center gap-x-1  bg-cyan-950 opacity-80 sm:inline-flex md:py-2 md:pl-2 md:pr-5 md:gap-x-2.5">
+            <div className="hidden absolute top-[35%] right-[7%] py-1 pl-1 pr-2 rounded-full justify-center items-center gap-x-1  bg-cyan-950 opacity-80 sm:inline-flex md:py-2 md:pl-2 md:pr-5 md:gap-x-2.5">
                 <div className="bg-sky-300 rounded-full pt-1 pr-1 pl-0.5 pb-0.5 inline-block md:p-2">
                     <img className="w-4 h-4" src={sentIcon} alt="sentIcon" />
                 </div>
